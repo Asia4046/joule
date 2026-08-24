93e1f9a006894f8c6286c1a1e02715c5e8babcf1
Asia4046 <kirito234875@gmail.com>
2026-08-24 22:32:28 +0530

    Fix metadata image issue

--- proxy.ts
+++ proxy.ts
@@ -22,5 +22,5 @@ export function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:png|svg|ico)).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:png|jpe?g|svg|ico|webp|gif)).*)"],
 };
